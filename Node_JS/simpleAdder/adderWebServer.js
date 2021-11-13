ff24d8d4c34bcd751e09931b97ceecafa3d1da37
diamondjuli <64670110+diamondjuli@users.noreply.github.com>
2021-11-13 00:36:45 -0600

    Simple Calculator

--- Node_JS/simpleAdder/adderWebServer.js
+++ Node_JS/simpleAdder/adderWebServer.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var addmod = require('./addmod.js');
-const { runInNewContext} = require('vm');
+//const { runInNewContext} = require('vm');
 http.createServer(function(req, res){
     var q = url.parse(req.url, true);
     var filename = "."+ q.pathname;
